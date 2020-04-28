9c664f846589a04b6ac88c75afddf24278d5468b
Stefan Paul Noack <stefan@noack.software>
2020-04-29 00:16:03 +0200

    speed improvement

--- loader.py
+++ loader.py
@@ -7,58 +7,49 @@ ser.baudrate = 62500
 ser.port     = '/dev/ttyUSB0'
 ser.stopbits = serial.STOPBITS_ONE
 ser.xonoff   = 0
+ser.rtscts   = 0
+ser.timeout  = 1
 
 ser.open()
 
-def set_address(address):
-    cmd = f'A{address:0{4}x}'
-    #print('\n'+cmd)
+def write_bytes(address, bytes):
+    count = len(bytes)
+    cmd = f'B{address:0{4}x}{count:0{4}x}'
     ser.write(cmd.encode())
-    assert ser.read(len(cmd)).decode() == cmd
-
-def read_bytes(count):
-    for _ in range(count):
-        ser.write(b'Rxx')
-    result=[]
-    for _ in range(count):
-        assert ser.read(1) == b'R'
-        data = int(ser.read(2), 16)
-        #print(f'R{data:0{2}x}', end=' ')
-        result.append(data)
-    return result
-
-def write_bytes(bytes):
-    for data in bytes:
-        cmd = f'W{data:0{2}x}'
-        #print(cmd, end=' ')
-        ser.write(cmd.encode())
-    for data in bytes:
-        cmd = f'W{data:0{2}x}'
-        assert ser.read(len(cmd)).decode() == cmd
+    ser.flush()
+    echo = ser.read(len(cmd)).decode()
+    if echo != cmd:
+        return None
+    ser.write(bytes)
+    ser.flush()
+    return list(ser.read(count))
 
 hex = IntelHex(sys.argv[1])
 
-print('READY', end='')
+print('READY')
 
 def chunks(seq, size):
     return (seq[pos:pos + size] for pos in range(0, len(seq), size))
 
 fails = 0
 for (start, stop) in hex.segments():
-    for chunk in chunks(range(start, stop), 64):
+    for chunk in chunks(range(start, stop), 1024):
         address = chunk[0]
         data = [hex[a] for a in chunk]
         print(f'WRITING {len(data)} BYTES AT ADDRESS ${address:0{4}x}')
         while True:
-            set_address(address)
-            write_bytes(data)
-            set_address(address)
-            check = read_bytes(len(data))
+            check = write_bytes(address, data)
             if check == data:
                 break
-            print("\n--- VERIFICATION ERROR - RETRYING ---", end="")
+            if check is None:
+                print(f"--- COMMAND ERROR - RETRYING ---")
+            elif len(check) != len(data):
+                print(f"--- LENGTH ERROR ({len(check)}) - RETRYING ---")
+            else:
+                print(f"--- VERIFICATION ERROR - RETRYING ---")
+
             fails+=1
 
 ser.close()
 
-print(f'\nDONE, {fails} RETRIES')
+print(f'DONE, {fails} RETRIES')
--- main.c
+++ main.c
@@ -1,21 +1,52 @@
 #include <avr/io.h>
+#include <avr/cpufunc.h>
 #include <util/setbaud.h>
 #include <util/delay.h>
 
 #include <ctype.h>
 #include <stdlib.h>
 
-#define SELECT_DDR DDRD
-#define SELECT PORTD
-#define NONE   0b01110000
-#define ADDR_L 0b00110100
-#define ADDR_H 0b00111000
-#define READ   0b00100000
-#define WRITE  0b00010000
+// PORT D
+// ======
+// - = unused
+// W = write strobe
+// S = select:
+//     AVR2313 -> 74AC138 -> Signal
+//     ----------------------------------
+//     000 (0) -> 000 (0) -> Latch CTRL
+//     001 (1) -> 100 (4) -> BUSRQ
+//     010 (2) -> 010 (2) -> Latch ADDR_H
+//     011 (3) -> 110 (6) -> N/A
+//     100 (4) -> 001 (1) -> Latch ADDR_L
+//     101 (5) -> 101 (5) -> RESET
+//     110 (6) -> 011 (3) -> Latch DATA
+//     111 (7) -> 111 (7) -> N/A
+// A = BUSACK (input)
+//               -WSSSA--
+#define IDLE   0b11111111
+#define ADDR_L 0b11100111
+#define ADDR_H 0b11010111
+#define DATA   0b11110111
+#define CTRL   0b11000111
+#define BUSRQ  0b11001111
+#define WSTRB  0b10001111
+#define RESET  0b11101111
+#define OUTS   0b00111000
+#define OUTSW  0b01111000
+#define BUSACK 0b00000100
 
-#define DATA_DDR DDRB
-#define DATA_OUT PORTB
-#define DATA_IN  PINB
+// CTRL
+// ====
+// - = unused
+// R = /RD
+// W = /WR
+// M = /MREQ
+// I = /IORQ
+//                  ----IMWR
+#define READ_MEM  0b11111010
+#define WRITE_MEM 0b11111001
+#define READ_IO   0b11110110
+#define WRITE_IO  0b11110101
 
 int uart_putc(unsigned char c)
 {
@@ -38,8 +69,6 @@ unsigned char uart_getc()
   while (!(UCSRA & (1<<RXC)));
   return UDR;
 }
-
-unsigned int address = 0;
 
 void print_nibble(unsigned char nibble)
 {
@@ -101,70 +130,106 @@ unsigned int input_hex_word()
   return (upper<<8) + lower;
 }
 
-void set_address(int addr)
+unsigned char read_byte(unsigned int address)
 {
-  DATA_DDR = 0xff;
-  SELECT = ADDR_H;
-//  _delay_us(1);
-  DATA_OUT = addr >> 8;
-//  _delay_us(1);
-  SELECT = ADDR_L;
-//  _delay_us(1);
-  DATA_OUT = addr;
-//  _delay_us(1);
-  SELECT = NONE;
-//  _delay_us(1);
-  DATA_DDR = 0x00;
-  DATA_OUT = 0xFF;
-  address = addr;
+  unsigned char result;
+  DDRB  = 0xff;
+
+  PORTB = READ_MEM;
+  PORTD = CTRL;
+  PORTD = IDLE;
+
+  PORTB = address & 0xff;
+  PORTD = ADDR_L;
+  PORTD = IDLE;
+
+  PORTB = (address>>8) & 0xff;
+  PORTD = ADDR_H;
+  PORTD = IDLE;
+
+  PORTB = 0xff;
+  DDRB  = 0x00;
+  PORTD = BUSRQ;
+
+  while (PIND & BUSACK);
+  result = PINB;
+  PORTD  = IDLE;
+
+  return result;
 }
 
-void read_byte()
+void write_byte(unsigned int address, unsigned char byte)
 {
-  DATA_DDR = 0x00;
-  DATA_OUT = 0xFF;
-  SELECT = READ;
-  _delay_us(1);
-  unsigned char byte = DATA_IN;
-  SELECT = NONE;
-  print_hex(byte);
-  set_address(address+1);
+  DDRB  = 0xff;
+
+  PORTB = WRITE_MEM;
+  PORTD = CTRL;
+  PORTD = IDLE;
+
+  PORTB = address & 0xff;
+  PORTD = ADDR_L;
+  PORTD = IDLE;
+
+  PORTB = (address>>8) & 0xff;
+  PORTD = ADDR_H;
+  PORTD = IDLE;
+
+  PORTB = byte;
+  PORTD = DATA;
+  PORTD = IDLE;
+
+  PORTB = 0xff;
+  DDRB  = 0x00;
+  PORTD = BUSRQ;
+
+  while (PIND & BUSACK);
+  DDRD  = OUTSW;
+  PORTD = WSTRB;
+  PORTD = BUSRQ;
+  DDRD  = OUTS;
+  PORTD = IDLE;
 }
 
-void write_byte(unsigned char byte)
+void load_block(unsigned int start, unsigned int size)
 {
-  DATA_DDR = 0xff;
-  SELECT = WRITE;
-  DATA_OUT = byte;
-//  _delay_us(1);
-  SELECT = NONE;
-//  _delay_us(1);
-  DATA_DDR = 0x00;
-  DATA_OUT = 0xFF;
-  set_address(address+1);
-}
-
-
-void get_address()
-{
-  print_hex(address>>8);
-  print_hex(address);
+  unsigned int i = size;
+  unsigned int addr = start;
+  unsigned char byte;
+  // write
+  while (i--)
+  {
+    byte = uart_getc();
+    write_byte(addr, byte);
+    addr++;
+  }
+  // verify
+  i = size;
+  addr = start;
+  while (i--)
+  {
+    byte = read_byte(addr);
+    uart_putc(byte);
+    addr++;
+  }
 }
 
 void main()
 {
+  unsigned int addr;
+  unsigned int size;
+
   UBRRH = UBRRH_VALUE;
   UBRRL = UBRRL_VALUE;
 #if USE_2X
   UCSRA = (1 << U2X);
 #endif
   UCSRC = (1<<UCSZ1) | (1<<UCSZ0);
-  UCSRB = (1 << TXEN) | (1 << RXEN);
+  UCSRB = (1<< TXEN) | (1<< RXEN);
 
-  SELECT_DDR = 0xff;
-  SELECT = NONE;
-
-  set_address(0);
+  DDRD  = OUTS;
+  PORTD = IDLE;
+  DDRB  = 0x00; // all inputs
+  PORTB = 0xFF; // pullups on
 
   while (1)
   {
@@ -172,23 +237,21 @@ void main()
     {
       case 'r':
         uart_putc('R');
-        read_byte();
+        addr = input_hex_word();
+        print_hex(read_byte(addr));
       break;
 
       case 'w':
         uart_putc('W');
-        write_byte(input_hex_byte());
+        addr = input_hex_word();
+        write_byte(addr, input_hex_byte());
       break;
 
-      case 'a':
-        uart_putc('A');
-        set_address(input_hex_word());
-      break;
-
-      case '?':
-        uart_putc('A');
-        get_address();
-      break;
+      case 'b':
+        uart_putc('B');
+        addr = input_hex_word();
+        size = input_hex_word();
+        load_block(addr, size);
     }
   }
 }
